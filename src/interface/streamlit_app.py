26a5261e48fff97f46deebbf27827e5464c3c1ab
Cole Kelley <Cole.Kelley@fcsamerica.com>
2023-10-21 19:54:10 -0500

    Minor fixes

--- src/interface/streamlit_app.py
+++ src/interface/streamlit_app.py
@@ -6,14 +6,16 @@ st.set_page_config(page_title="Home",
 
 st.header("Happy Food Company", divider='blue')
 
-col1, col2 = st.columns(2)
+# col1, col2 = st.columns(2)
+#
+# with col1:
+#     st.markdown('![Pizza Cabin.](app/static/pizza_cabin_logo.png)')
+#
+# with col2:
+#     st.markdown('![Taco Shell.](app/static/taco_shell_logo.png)')
 
-with col1:
-    st.markdown('![Pizza Cabin.](app/static/pizza_cabin_logo.png)')
-
-with col2:
-    st.markdown('![Taco Shell.](app/static/taco_shell_logo.png)')
-
+st.markdown('![Pizza Cabin.](app/static/pizza_cabin_logo.png)')
+st.markdown('![Taco Shell.](app/static/taco_shell_logo.png)')
 st.markdown('![Tennessee Baked Chicken.](app/static/tennessee_baked_chicken_logo.png)')
 
 st.sidebar.markdown("# Home")
